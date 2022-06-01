fc83026432845a63958d44e19ed6d1f95aed5a7a
Nolan Reda <nolan.reda@gmail.com>
2022-06-01 03:15:58 +0000

    completed functionality

--- js/data.js
+++ js/data.js
@@ -6,3 +6,26 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+// var $title = document.querySelector('#title');
+// var $urlInput = document.querySelector('#URL-input');
+// var $notes = document.querySelector('#notes');
+// var $imgBox = document.querySelector('#img-box');
+// var $save = document.querySelector('.save');
+
+// function renderEntry(entry) {
+//   var newDiv = document.createElement('div');
+// }
+
+var previousEntriesJSON = localStorage.getItem('journal');
+
+if (previousEntriesJSON !== null) {
+  data = (JSON.parse(previousEntriesJSON));
+}
+
+function handleUnload(event) {
+  var jsonEntry = JSON.stringify(data);
+  localStorage.setItem('journal', jsonEntry);
+}
+
+window.addEventListener('beforeunload', handleUnload);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-// var $title = document.querySelector('#title');
-// var $photoURL = document.querySelector('#photo-url');
-// var $notes = document.querySelector('#notes');
+/* global data */
+
+var $title = document.querySelector('#title');
+var $urlInput = document.querySelector('#URL-input');
+var $notes = document.querySelector('#notes');
+var $imgBox = document.querySelector('#img-box');
+var $form = document.querySelector('form');
+
+function changeSrc(event) {
+  $imgBox.src = $urlInput.value;
+}
+
+$urlInput.addEventListener('input', changeSrc);
+
+function handleSubmit(event) {
+  var newEntry = {};
+  newEntry.image = $urlInput.value;
+  newEntry.title = $title.value;
+  newEntry.notes = $notes.value;
+  newEntry.nextEntryId = data.nextEntryId;
+  data.entries.push(newEntry);
+  data.nextEntryId++;
+}
+
+$form.addEventListener('submit', handleSubmit);
